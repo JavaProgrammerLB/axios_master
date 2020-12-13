c1a3da4409953be18118b93713272cef2920e66c
刘备 <admin@yitianyigexiangfa.com>
2020-12-13 13:00:21 +0800

    实现拦截器打印出每次request的日志

--- main.js
+++ main.js
@@ -1,20 +1,88 @@
-function getSth(){
+axios.interceptors.request.use(
+    config => {
+        console.log(
+            `${config.method.toUpperCase()} request sent to ${config.url} at ${(new Date()).toLocaleString()}`
+            );
+        return config;
+    }, error => {
+        return Promise.reject(error);
+    }
+);
+
+function getSth() {
     axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    .then(function(response) {
-        showResponse(response)
-    })
-    .catch(function(error){
-        alert(error)
-    });
-}
-  
-function postSth(a, b){
-    alert("post a and b")
+        .then(function (response) {
+            showResponse(response)
+        })
+        .catch(function (error) {
+            alert(error)
+        });
 }
 
-function showResponse(response){
-    if(response){
-        document.getElementById("response").innerHTML =`
+function getAll() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(function (response) {
+            showResponse(response)
+        })
+        .catch(function (error) {
+            alert(error)
+        });
+}
+
+function getByUserId() {
+    axios.get("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+            "userId": 10
+        }
+    })
+        .then(function (response) {
+            showResponse(response)
+        }).catch(function (error) {
+            alert(error)
+        })
+}
+
+function postSth() {
+    axios.post('https://jsonplaceholder.typicode.com/posts', {
+        'title': 'BBBBBBBBBBBB',
+        'body': 'LLLLLLLLLLLLL',
+        'userId': 1
+    })
+        .then(function (response) {
+            showResponse(response)
+        })
+        .catch(function (error) {
+            alert(error)
+        });
+}
+
+function putSth() {
+    axios.put('https://jsonplaceholder.typicode.com/posts/1', {
+        'title': 'BBBBBBBB',
+        'userId': 1,
+        'id': 101
+    })
+        .then(function (response) {
+            showResponse(response)
+        })
+        .catch(function (error) {
+            alert(error)
+        });
+}
+
+function deleteById(index) {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+        .then(function (response) {
+            showResponse(response)
+        })
+        .catch(function (error) {
+            alert(error)
+        });
+}
+
+function showResponse(response) {
+    if (response) {
+        document.getElementById("response").innerHTML = `
         <div>
         <h4>Response Code : ${response.status}</h4>
     </div>
@@ -38,5 +106,5 @@ ${JSON.stringify(response.config, null, 2)}
     </div
         `
     }
-    
+
 }
